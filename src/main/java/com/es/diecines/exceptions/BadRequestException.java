d3904b3fe3650e4fe7a5a86fe948690d47455c0d
Camilleri <ssancam391@g.educaand.es>
2024-11-21 20:52:43 +0100

    quicksave

---
+++ src/main/java/com/es/diecines/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.es.diecines.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/es/diecines/exceptions/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.es.diecines.exceptions;
+
+public class NotFoundException {
+}
